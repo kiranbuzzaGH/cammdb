5978a1d67b1316fd133991e94712b121d66aa28e
KiranBuzzaGH <knb34@cantab.ac.uk>
2025-04-08 18:16:11 +0100

    Configured sessions. Defined load_user function.

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from models import engine
+from flask import Flask, g, render_template, request, session
+from flask_session import Session
+from flask_talisman import Talisman
+from redis import Redis
 from sqlalchemy import text
 
+from helpers import login_required
+from models import engine
+
 app = Flask(__name__)
+
+# Configure session to use redis backend to store session -
+# https://flask-session.readthedocs.io/en/latest/usage.html#quickstart
+SESSION_TYPE = 'redis'
+SESSION_REDIS = Redis(host="localhost", port=6379)
+app.config.from_object(__name__)
+Session(app)
+
+# Wrap app with a Talisman to protect against security issues - currently
+# default (most secure) settings
+Talisman(app)
+
+# Control configuration values - https://flask.palletsprojects.com/en/stable/config/
+# In particular, app.config["SECRET_KEY"] has been assigned
+app.config.from_prefixed_env()
+
+
+@app.before_request
+def load_user():
+    g.user = None
+    user_id = session.get("user_id")
+    if user_id:
+        with engine.connect() as conn:
+            stmt = text("SELECT * FROM users where id = :user_id")
+            result = conn.execute(stmt, {"user_id": user_id}).first()
+            if result:
+                g.user = dict(result._mapping)
+            else:
+                # Clear invalid session
+                session.pop("user_id", None)
 
 
 @app.route("/")
@@ -41,11 +74,16 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        # TODO: login logic
+
+        # Regenerate session identifier when user logs in to prevent session fixation
+        app.session_interface.regenerate(session)
         return "Login in production"
     else:
         return "Login in production"
 
 @app.route("/user/<username>")
+@login_required
 def user_profile(username):
     """Show the user profile for given user"""
     return "User page in production"
---
+++ helpers.py
@@ -0,0 +1,13 @@
+"""Miscelaneous functions are defined here."""
+
+from functools import wraps
+from flask import g, request, redirect, url_for
+
+def login_required(f):
+    """Decorate routes to require login."""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
