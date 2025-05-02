eef127d797e0caf9791e06ff7aa768bbd2bc20d4
KiranBuzzaGH <knb34@cantab.ac.uk>
2025-05-02 09:58:41 +0100

    Remove out of date files

--- cammdb/__init__.py
+++ cammdb/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_seasurf import SeaSurf
+from flask_talisman import Talisman
 
 from . import db
 from . import auth
@@ -16,6 +18,15 @@ load_dotenv()
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    # Prevent cross site request forgery
+    # Docs: https://flask-seasurf.readthedocs.io/en/latest/
+    csrf = SeaSurf(app)
+
+    # Wrap app with a Talisman to protect against security issues - currently default (most secure)
+    # settings
+    # Docs: https://github.com/GoogleCloudPlatform/flask-talisman
+    Talisman(app)
 
     app.config.from_mapping(DATABASE=os.path.join(app.instance_path, "cammdb.sqlite"),)
 
